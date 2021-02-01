b6aadec3e91172b009ec474e298cd14a07101755
Christian Arnheiter <christian.arnheiter@nttdata.com>
2021-02-01 10:27:19 +0100

    http handler added

--- http.go
+++ http.go
@@ -10,6 +10,54 @@ import (
 
 type ErrorHandler func(http.ResponseWriter, error) error
 
+type AuthHandler struct {
+	verifier     *JwtVerifier
+	claimHandler ClaimHandler
+	errorHandler ErrorHandler
+	handler      http.Handler
+}
+
+func NewAuthHandler(verifier *JwtVerifier, claimHandler ClaimHandler, errorHandler ErrorHandler) func(h http.Handler) http.Handler {
+	// no JWT verifier, so just pass it to the next handler without any verification
+	if verifier == nil {
+		return func(handler http.Handler) http.Handler {
+			return handler
+		}
+	}
+
+	return func(h http.Handler) http.Handler {
+		a := AuthHandler{
+			verifier:     verifier,
+			claimHandler: claimHandler,
+			errorHandler: errorHandler,
+			handler:      h,
+		}
+		return a
+	}
+}
+func (a AuthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	bearerToken, err := GetBearerToken(*r)
+	if err != nil {
+		r.Header.Set("userId", "anonymous")
+	}
+	// JWT verification means authentication
+	claims, err := a.verifier.Validate(bearerToken)
+	// pass the result to the error handler
+	if a.errorHandler(w, err) != nil {
+		return
+	}
+	// pass the claims to the claim handler
+	if a.claimHandler != nil {
+		err = a.claimHandler(claims, r, w)
+		if err != nil {
+			return
+		}
+	}
+	// if error handler and claim handler returned no error we can
+	// safely pass the request to the next handler
+	a.handler.ServeHTTP(w, r)
+}
+
 func HttpTokenHandler(verifier *JwtVerifier, claimHandler ClaimHandler, errorHandler ErrorHandler, h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// no JWT verifier, so just pass it to the next handler without any verification
